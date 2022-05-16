4dfeef122d82b7d1dd9771658290150cdb0097b0
Victor Moragues <30898324+vicmova@users.noreply.github.com>
2022-05-16 16:06:54 +0200

    Add files via upload

---
+++ gestv.c
@@ -0,0 +1,71 @@
+/*25 mayo = posible fecha de exámen;
+
+crear vecttos num con 3 posiciones inicializado a 0, es el um de registros q tengo en la fila de la matriz. 
+ir al fichero y leemos la posición.
+
+scanf 1 leer operador
+scanf 2
+a[ope[num[ope]]]
+num[ope]++;
+función mostrar=también por filas
+
+V A[3][NMAX] = DEFINIR MATRIZ
+
+*/
+
+#include<stdio.h>
+
+int main(){
+
+ int n=1;
+ 
+ 
+ 	while(n!=0){
+ 	
+ 		printf("-----------------------------------------------------------\n");	
+		printf("\n");
+		printf("   Selecciona una opción.");
+		printf("\n\n");
+ 	
+ 		printf("Carga ventas desde fichero. \n");
+ 		
+ 		printf("Introduce venta. \n");
+ 		
+ 		printf("Muestra información de un cliente. \n");
+ 		
+ 		printf("Muestra información de todos los clientes. \n");
+ 		
+ 		printf("Dar de baja a un cliente. \n");
+ 		
+ 		printf("****. \n");
+ 		
+ 		printf("Salir del programa con confirmación. \n");
+ 		
+ 		printf("\n");
+		printf("-----------------------------------------------------------\n");
+ 		
+ 		
+ 		
+ 		scanf("%d", &n);
+ 		printf("\n");
+ 	
+ 	
+ 	
+ 	}
+ 	
+ 	switch(n){
+ 	
+ 		case 0: printf("BYE! \n");
+ 		
+ 		break;
+ 		
+ 		case 1:
+ 		
+ 		default: printf("Opción incorrecta. \n");
+ 	
+ 	
+ 	
+ 	}
+
+
+}
