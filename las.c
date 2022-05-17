b7accff64b9e1e290ac6c2d5598f4549d929cd97
Victor Moragues <30898324+vicmova@users.noreply.github.com>
2022-05-17 17:22:19 +0200

    Add files via upload

---
+++ las.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+
+#define nmax 100
+
+	typedef struct Ventas{
+
+			char tlf[9];
+			char cliente[40];
+			int tipoContrato;
+
+			}venta;
+
+
+int cargar(venta A[][nmax]);
+
+
+int main(){
+
+ 	int n=1, i, j;
+	int operadora[3]={0,0,0};
+	venta A[3][nmax];
+	
+	FILE *f=fopen("fi.txt", "r");
+
+				
+			if(f==NULL){
+	
+				return -1;			
+			
+				} else {				
+
+				for(i=0;i<7;i++){
+					
+		
+						fscanf(f,"%d", &operadora[i]);
+				
+				}
+								
+
+					for(i=0;i<7;i++){
+						
+						
+		
+						printf("valor de la operadora[%d]: %d \n",i, operadora[i]);
+				
+
+					}			
+
+	fclose (f);
+					
+
+
+				}
+}
+
+
+		
+		
+
+		
+
+
+		
+		
